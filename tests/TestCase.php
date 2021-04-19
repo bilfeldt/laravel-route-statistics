b7d211b127f0ca98c6cc1545e56984bee209d271
bilfeldt <bilfeldt@users.noreply.github.com>
2021-04-19 10:04:12 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,9 @@
 
 namespace Bilfeldt\LaravelRouteStatistics\Tests;
 
+use Bilfeldt\LaravelRouteStatistics\LaravelRouteStatisticsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
-use Bilfeldt\LaravelRouteStatistics\LaravelRouteStatisticsServiceProvider;
 
 class TestCase extends Orchestra
 {
