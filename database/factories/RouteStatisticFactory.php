83c0eb45bae94056feebd703c60371045b354db2
Maria E Kibtia <74188241+MariaEkibtia@users.noreply.github.com>
2023-02-28 11:24:46 +0100

    Update RouteStatisticFactory.php
    Some modification in the attributes and faker functions.

--- database/factories/RouteStatisticFactory.php
+++ database/factories/RouteStatisticFactory.php
@@ -12,11 +12,12 @@ class RouteStatisticFactory extends Factory
     public function definition()
     {
         return [
-            'method' => $this->faker->randomElements(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']),
-            'route'  => $this->faker->domainWord().'.'.$this->faker->randomElements(['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']),
-            'code'   => $this->faker->randomElement([200, 201, 202, 204, 300, 301, 302, 303, 304, 400, 401, 402, 403, 404, 405, 406, 422, 429, 500, 501, 502, 503, 504]),
-            'ip'     => $this->faker->ipv4,
-            'date'   => $this->faker->dateTime,
+            'method' => $this->faker->randomElement(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']),
+            'route' => $this->faker->domainWord().'.'.$this->faker->randomElement(['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']),
+            'status' => $this->faker->randomElement([200, 201, 202, 204, 300, 301, 302, 303, 304, 400, 401, 402, 403, 404, 405, 406, 422, 429, 500, 501, 502, 503, 504]),
+            'ip' => $this->faker->ipv4,
+            'date' => $this->faker->dateTime,
+            'counter' => $this->faker->randomNumber(4),
         ];
     }
 }
