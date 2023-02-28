4a7786c9e9c4da8116b7cfa1c8b0b50cc8d87844
Maria E Kibtia <74188241+MariaEkibtia@users.noreply.github.com>
2023-02-28 11:49:28 +0100

    Update RouteStatisticFactory.php

--- database/factories/RouteStatisticFactory.php
+++ database/factories/RouteStatisticFactory.php
@@ -15,8 +15,8 @@ class RouteStatisticFactory extends Factory
             'method' => $this->faker->randomElement(['GET', 'POST', 'PUT', 'PATCH', 'DELETE']),
             'route' => $this->faker->domainWord().'.'.$this->faker->randomElement(['index', 'create', 'store', 'show', 'edit', 'update', 'destroy']),
             'status' => $this->faker->randomElement([200, 201, 202, 204, 300, 301, 302, 303, 304, 400, 401, 402, 403, 404, 405, 406, 422, 429, 500, 501, 502, 503, 504]),
-            'ip' => $this->faker->ipv4,
-            'date' => $this->faker->dateTime,
+            'ip' => $this->faker->ipv4(),
+            'date' => $this->faker->dateTime(),
             'counter' => $this->faker->randomNumber(4),
         ];
     }
