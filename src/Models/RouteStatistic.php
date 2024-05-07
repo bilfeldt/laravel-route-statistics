58bd78db75560b4f2feccc5a1e05f3608c3fbac8
Bilfeldt <abilfeldt@gmail.com>
2024-05-07 22:20:10 +0200

    Update RouteStatistic.php
    Revert relationship changes

--- src/Models/RouteStatistic.php
+++ src/Models/RouteStatistic.php
@@ -53,7 +53,7 @@ class RouteStatistic extends Model implements RequestLoggerInterface
 
     public function user(): BelongsTo
     {
-        return $this->belongsTo(config('route-statistics.user_model'));
+        return $this->belongsTo(config('auth.providers.users.model'));
     }
 
     public function team(): BelongsTo
